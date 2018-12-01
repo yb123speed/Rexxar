fbd7dd4fb249e344d63106d75c200e43fac666ae
yb123speed <904896209@qq.com>
2018-12-01 15:02:36 +0800

    create LoginPage

---
+++ Rexxar.Frontend/Rexxar.Xamarin/Rexxar.Xamarin/Views/LoginPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Rexxar.Xamarin.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        void LoginBtn_Clicked(object sender, System.EventArgs e)
+        {
+            var uname = (username as Entry).Text;
+            var pwd = (password as Entry).Text;
+
+        }
+    }
+}
